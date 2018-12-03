7042148f62e78ee4aa7c841468d8ac59f9e8e387
Tiffany Cuneo <tiffanycuneo@gmail.com>
2018-12-03 11:35:12 -0700

    finished html

---
+++ assets/javascript/tiffanys.js
@@ -0,0 +1,20 @@
+var map;
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+
+        zoom: 7,
+        center: { lat: 28.417831662, lng: -81.575331032 },
+
+    });
+    var marker = new google.maps.Marker({
+        position: { lat: 28.417831662, lng: -81.575331032 },
+        map: map,
+        title: "Hello Mickey Mouse!"
+    });
+
+    var marker = new google.maps.Marker({
+        position: { lat: 28.5729, lng: -80.6490 },
+        map: map,
+        title: "Space Shuttle!"
+    });
+}
